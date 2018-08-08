90d3d53dfc7185fcbf4731f3d668235be4cdf231
srap5664 <srap5664@gmail.com>
2018-08-08 16:07:51 +0300

    adding signUp page

--- src/SignUp.js
+++ src/SignUp.js
@@ -5,16 +5,25 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 class SignUp extends Component {
   render() {
     return (
-     <div>
-       
-            {/*  here add jsx code for SignUp page (if you need to add css do it in SignUp.css)
-            -the attribute of any tag should be "className" instead of "class" because this is jsx not html ;)
-            -as you can see bootstrap is already added to this page just use any bootstrap classes and make your life easy beasy       
-            -the code you will write it will goes in the middle of the page I took care of it ;)
-            -don't worry about the header or the footer it will be added automaticly    
-           */} 
-
-     </div>  
+      
+    <div className="span3 SignUp">
+      <h2>Sign Up</h2>
+      <form>
+        <label>First Name</label>
+        <input type="text" name="firstname" className="span3" />
+        <label>Last Name</label>
+        <input type="text" name="lastname" className="span3" />
+        <label>Email Address</label>
+        <input type="email" name="email" className="span3" />
+        <label>Username</label>
+        <input type="text" name="username" className="span3" />
+        <label>Password</label>
+        <input type="password" name="password" className="span3" /> 
+        <label><input type="checkbox" name="terms" /> I agree with the <a href="#">Terms and Conditions</a>.</label>
+        <input type="submit" value="Sign up" className="btn btn-primary pull-right" />
+        <div className="clearfix"></div>
+      </form>
+    </div> 
         
     );
   }
