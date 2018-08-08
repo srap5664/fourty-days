63da7cb6f3b418baa04e5f9f9504382ed2c61956
Alanoud Saad <36537989+a3nodq@users.noreply.github.com>
2018-08-08 23:41:23 +0300

    Add SignUp.js psge

--- src/SignUp.js
+++ src/SignUp.js
@@ -5,7 +5,6 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 class SignUp extends Component {
   render() {
     return (
-      
     <div className="span3 SignUp">
       <h2>Sign Up</h2>
       <form>
@@ -25,6 +24,60 @@ class SignUp extends Component {
       </form>
     </div> 
         
+href="#" class="btn btn-lg btn-info btn-block">Google</a>
+         </div>
+       </div>
+       <form action="">
+         <div className="form-group center">
+          <label for="username" className="col-4 col-form-label">أسم المستخدم*</label>
+          <div className="form-group center">
+            <input id="username" name="username" placeholder="أسم المستخدم" className="form-control here" required="required" type="text" />
+          </div>
+        </div>
+        <div className="form-group">
+          <label for="name" className="col-4 col-form-label">الأسم الأول*</label>
+          <div className="center">
+            <input id="name" name="name" align="center" placeholder="الأسم الأول" className="form-control here" type="text" />
+          </div>
+        </div>
+        <div className="form-group">
+          <label for="lastname" className="col-4 col-form-label">الأسم الأخير</label>
+          <div className="form-group">
+            <input id="lastname" name="lastname" placeholder="الأسم الأخير" className="form-control here" type="text"/>
+          </div>
+        </div>
+        <div className="form-group">
+          <label for="text" className="col-4 col-form-label">الكنية*</label>
+          <div className="form-group">
+            <input id="text" name="text" placeholder="الكنية" className="form-control here" required="required" type="text" />
+          </div>
+        </div>
+        <div className="form-group">
+          <label for="email" className="col-4 col-form-label">البريد الإلكتروني*</label>
+          <div className="form-group">
+            <input id="email" name="email" placeholder="example@example.com*" className="form-control here" required="required" type="text" />
+          </div>
+        </div>
+        <div className="form-group">
+          <label for="newpass" className="col-4 col-form-label" type="password">كلمة المرور*</label>
+          <div className="form-group">
+            <input id="newpass" name="newpass" placeholder="يجب أن تتكون من 8 رموز" className="form-control here" type="password" />
+          </div>
+        </div>
+        <div className="form-group">
+          <label for="newpass" className="col-4 col-form-label">تأكيد لكلمة المرور* </label>
+          <div className="form-group">
+            <input id="newpass" type="password" name="newpass" placeholder="يجب أن تطابق كلمة المرور" className="form-control here" type="password" />
+          </div>
+        </div>
+        <div className="form-group">
+          <div className="form-group col-15">
+            <button name="submit" type="submit" className="btn btn-primary" align="center" >تسجيـــل</button>
+          </div>
+        </div>
+       </form>
+     </div>
+
     );
   }
 }
