aba83e06701941c7eee6dd9f864f5eee9ff552e6
srap5664 <srap5664@gmail.com>
2018-08-08 15:30:31 +0300

    fixing profile page

--- src/Profile.js
+++ src/Profile.js
@@ -7,64 +7,56 @@ class Profile extends Component {
     return (     
    <form action="">
      <div className="form-group row">
-      <label for="username" className="col-4 col-form-label">User Name*</label> 
+      <label for="username" className="col-4 col-form-label">أسم المستخدم*</label> 
       <div className="col-8">
-        <input id="username" name="username" placeholder="Username" className="form-control here" required="required" type="text" />
+        <input id="username" name="username" placeholder="أسم المستخدم" className="form-control here" required="required" type="text" />
       </div>
     </div>
     <div className="form-group row">
-      <label for="name" className="col-4 col-form-label">First Name</label> 
+      <label for="name" className="col-4 col-form-label">الأسم الأول</label> 
       <div className="col-8">
-        <input id="name" name="name" placeholder="First Name" className="form-control here" type="text" />
+        <input id="name" name="name" placeholder="الأسم الأول" className="form-control here" type="text" />
       </div>
     </div>
     <div className="form-group row">
-      <label for="lastname" className="col-4 col-form-label">Last Name</label> 
+      <label for="lastname" className="col-4 col-form-label">الأسم الأخير</label> 
       <div className="col-8">
-        <input id="lastname" name="lastname" placeholder="Last Name" className="form-control here" type="text"/>
+        <input id="lastname" name="lastname" placeholder="الأسم الأخير" className="form-control here" type="text"/>
       </div>
     </div>
     <div className="form-group row">
-      <label for="text" className="col-4 col-form-label">Nick Name*</label> 
+      <label for="text" className="col-4 col-form-label">الكنية*</label> 
       <div className="col-8">
-        <input id="text" name="text" placeholder="Nick Name" className="form-control here" required="required" type="text" />
+        <input id="text" name="text" placeholder="الكنية" className="form-control here" required="required" type="text" />
       </div>
     </div>
     <div className="form-group row">
-      <label for="select" className="col-4 col-form-label">Display Name public as</label> 
+      <label for="email" className="col-4 col-form-label">البريد الإلكتروني*</label> 
       <div className="col-8">
-        <select id="select" name="select" className="custom-select">
-          <option value="admin">Admin</option>
-        </select>
+        <input id="email" name="email" placeholder="البريد الإلكتروني" className="form-control here" required="required" type="text" />
       </div>
     </div>
     <div className="form-group row">
-      <label for="email" className="col-4 col-form-label">Email*</label> 
+      <label for="website" className="col-4 col-form-label">الموقع</label> 
       <div className="col-8">
-        <input id="email" name="email" placeholder="Email" className="form-control here" required="required" type="text" />
+        <input id="website" name="website" placeholder="الموقع" className="form-control here" type="text" />
       </div>
     </div>
     <div className="form-group row">
-      <label for="website" className="col-4 col-form-label">Website</label> 
-      <div className="col-8">
-        <input id="website" name="website" placeholder="website" className="form-control here" type="text" />
-      </div>
-    </div>
-    <div className="form-group row">
-      <label for="publicinfo" className="col-4 col-form-label">Public Info</label> 
+      <label for="publicinfo" className="col-4 col-form-label">معلومات عامة</label> 
       <div className="col-8">
         <textarea id="publicinfo" name="publicinfo" cols="40" rows="4" className="form-control"></textarea>
       </div>
     </div>
     <div className="form-group row">
-      <label for="newpass" className="col-4 col-form-label">New Password</label> 
+      <label for="newpass" className="col-4 col-form-label">كلمة مرور جديدة</label> 
       <div className="col-8">
-        <input id="newpass" name="newpass" placeholder="New Password" className="form-control here" type="text" />
+        <input id="newpass" name="newpass" placeholder="كلمة مرور جديدة" className="form-control here" type="text" />
       </div>
     </div> 
     <div className="form-group row">
       <div className="offset-4 col-8">
-        <button name="submit" type="submit" className="btn btn-primary">Update My Profile</button>
+        <button name="submit" type="submit" className="btn btn-primary">تحديث الصفحة الشخصيه</button>
       </div>
     </div>
    </form>
